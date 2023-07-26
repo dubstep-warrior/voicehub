1c761a348c410c170e4ef15ae1952cf1b69a98c4
Bo Jin Kok <75065469+dubstep-warrior@users.noreply.github.com>
2023-07-26 14:10:37 +0800

    Clean up input component

--- shared/Input.tsx
+++ shared/Input.tsx
@@ -3,17 +3,26 @@ import { TextInput } from "react-native-gesture-handler";
 import { StyleSheet } from "react-native";
 import { View } from "react-native";
 
-export default function Input(props: Props) {
+interface IInput {
+  name: string;
+  handleFormValueChange: any; 
+  textInputProps?: any;
+  formKey: string
+}
+
+export default function Input(props: IInput) {
+  const { name, handleFormValueChange, formKey, ...textInputProps } = props;
+
   return (
-    <View style={{width: '100%'}}> 
+    <View style={{ width: "100%" }}>
       <TextInput
-        placeholder={props.name}
+        placeholder={name}
         style={styles.input}
         onChange={(event) =>
-          props.handleFormValueChange(props.formKey, event.nativeEvent.text)
+          handleFormValueChange(formKey, event.nativeEvent.text)
         }
-        {...props.textInputProps}
-      /> 
+        {...textInputProps}
+      />
     </View>
   );
 }
