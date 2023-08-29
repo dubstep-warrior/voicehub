f73f0076aecb7ab1562378880d33b9e8b516c4c7
Bo Jin Kok <75065469+dubstep-warrior@users.noreply.github.com>
2023-08-29 21:21:11 +0800

    minor updates

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import { store } from "./src/store/store";
 import { Provider } from "react-redux";
 import VoiceHub from "./src/VoiceHub";
 import 'react-native-gesture-handler'
+import 'expo-dev-client';
+
  
 export default function App() {
   return (
